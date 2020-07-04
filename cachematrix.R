98451b0ca3b3620229dbe198136ec744deb73008
DaniloAugustoAM <67844365+DaniloAugustoAM@users.noreply.github.com>
2020-07-04 18:11:44 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## With these two functions we can cache a matrix and its inverse into an environment different to the current
+##This following function allow setting and getting the original matrix and its inverse into a list
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+  inv<<-NULL
+  }
+  get<-function(){x}
+  setInve<-function(inve){inv<<-inve}
+  getInve<-function(){inv}
+  list(set=set,get=get,setInve=setInve,getInve=getInve)
 }
 
-
-## Write a short comment describing this function
-
+## This function firstly verifies if the inverse of a matrix has already been found to give it, if not
+##it allows found it so the cached it.
 cacheSolve <- function(x, ...) {
+  inv<-x$getInve()
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  matriz<-x$get()
+  inv<-solve(matriz,...)
+  x$setInve(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
